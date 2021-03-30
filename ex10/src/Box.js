2995e754f2ee73ae461c6e0f279684afecb245ae
jpjanic <jasmin.pjanic@yahoo.com>
2021-03-30 05:24:08 +0200

    Added React files

---
+++ ex10/src/Box.js
@@ -0,0 +1,11 @@
+import  React from "react";
+
+// example simple component 
+
+function Box(props) {
+    // Only change code below this line
+        return <h2>This is a {props.size}px Box!</h2>;
+    // Only change code above this line
+}
+
+export default Box;
